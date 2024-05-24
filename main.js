9cdf6425cd20dcc392158b10e1064c545677fd70
blvckcvt7 <134386774+blvckcvt7@users.noreply.github.com>
2024-05-25 08:15:29 +0900

    Complete the functionality of the game
    Created a loop within playGame to play 5-rounds

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 'use strict';
 
-let computerScore = 0;
-let humanScore = 0;
-
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3 + 1);
 
@@ -23,3 +20,46 @@ function getHumanChoice() {
 }
 
 // console.log(getHumanChoice());
+
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+  function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    if (computerChoice == 'paper' && humanChoice == 'rock') {
+      computerScore++;
+      return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
+    } else if (computerChoice == 'rock' && humanChoice == 'scissors') {
+      computerScore++;
+      return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
+    } else if (computerChoice == 'scissors' && humanChoice == 'paper') {
+      computerScore++;
+      return `Computer wins! ${computerChoice} beats ${humanChoice}.`;
+    } else if (computerChoice == humanChoice) {
+      return `It's a tie! Both players selected ${computerChoice}!`;
+    } else {
+      humanScore++;
+      return `Human wins! ${humanChoice} beats ${computerChoice}.`;
+    }
+  }
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+  }
+  if (computerScore > humanScore) {
+    console.log(
+      `Computer wins with a score of ${computerScore} vs ${humanScore}.`
+    );
+  } else if (humanScore > computerScore) {
+    console.log(
+      `Human wins with a score of ${humanScore} vs ${computerScore}.`
+    );
+  } else {
+    console.log(
+      `It's a tie! both players scored ${humanScore}! Play another game.`
+    );
+  }
+}
+
+playGame();
