113a8e39cf29855769b7a05f9a8dffdb73d7507b
blvckcvt7 <134386774+blvckcvt7@users.noreply.github.com>
2024-05-25 07:22:55 +0900

    Creates conditional to get computer choice
    Creates conditions within getComputerChoice

--- main.js
+++ main.js
@@ -2,7 +2,14 @@
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3 + 1);
-  return num;
+
+  if (num == 1) {
+    return 'rock';
+  } else if (num == 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 }
 
 console.log(getComputerChoice());
